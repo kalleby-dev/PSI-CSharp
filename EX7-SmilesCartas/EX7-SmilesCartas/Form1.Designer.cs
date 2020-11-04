6beebd4e32276eb8f0be1a0290f01c6bf3a1c0d6
kallebysantos <60856996+kallebysantos@users.noreply.github.com>
2020-11-04 08:55:51 +0000

    feat: Interface Smile and cards

--- EX7-SmilesCartas/EX7-SmilesCartas/Form1.Designer.cs
+++ EX7-SmilesCartas/EX7-SmilesCartas/Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.smilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cartasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.escolherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.escolherToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnHappy = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSad = new System.Windows.Forms.ToolStripMenuItem();
+            this.espadasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ouroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pausToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smilesToolStripMenuItem,
+            this.cartasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(729, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // smilesToolStripMenuItem
+            // 
+            this.smilesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.escolherToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.smilesToolStripMenuItem.Name = "smilesToolStripMenuItem";
+            this.smilesToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.smilesToolStripMenuItem.Text = "Smiles";
+            // 
+            // cartasToolStripMenuItem
+            // 
+            this.cartasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.escolherToolStripMenuItem1,
+            this.sairToolStripMenuItem1});
+            this.cartasToolStripMenuItem.Name = "cartasToolStripMenuItem";
+            this.cartasToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.cartasToolStripMenuItem.Text = "Cartas";
+            // 
+            // escolherToolStripMenuItem
+            // 
+            this.escolherToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnHappy,
+            this.btnSad});
+            this.escolherToolStripMenuItem.Name = "escolherToolStripMenuItem";
+            this.escolherToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.escolherToolStripMenuItem.Text = "Escolher";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // escolherToolStripMenuItem1
+            // 
+            this.escolherToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.espadasToolStripMenuItem,
+            this.copasToolStripMenuItem,
+            this.ouroToolStripMenuItem,
+            this.pausToolStripMenuItem});
+            this.escolherToolStripMenuItem1.Name = "escolherToolStripMenuItem1";
+            this.escolherToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.escolherToolStripMenuItem1.Text = "Escolher";
+            // 
+            // sairToolStripMenuItem1
+            // 
+            this.sairToolStripMenuItem1.Name = "sairToolStripMenuItem1";
+            this.sairToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.sairToolStripMenuItem1.Text = "Sair";
+            // 
+            // btnHappy
+            // 
+            this.btnHappy.Name = "btnHappy";
+            this.btnHappy.Size = new System.Drawing.Size(224, 26);
+            this.btnHappy.Text = "Feliz";
+            // 
+            // btnSad
+            // 
+            this.btnSad.Name = "btnSad";
+            this.btnSad.Size = new System.Drawing.Size(224, 26);
+            this.btnSad.Text = "Triste";
+            // 
+            // espadasToolStripMenuItem
+            // 
+            this.espadasToolStripMenuItem.Name = "espadasToolStripMenuItem";
+            this.espadasToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.espadasToolStripMenuItem.Text = "espadas";
+            // 
+            // copasToolStripMenuItem
+            // 
+            this.copasToolStripMenuItem.Name = "copasToolStripMenuItem";
+            this.copasToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.copasToolStripMenuItem.Text = "copas";
+            // 
+            // ouroToolStripMenuItem
+            // 
+            this.ouroToolStripMenuItem.Name = "ouroToolStripMenuItem";
+            this.ouroToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ouroToolStripMenuItem.Text = "ouro";
+            // 
+            // pausToolStripMenuItem
+            // 
+            this.pausToolStripMenuItem.Name = "pausToolStripMenuItem";
+            this.pausToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pausToolStripMenuItem.Text = "paus";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(164, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 401);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(729, 512);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem smilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem escolherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnHappy;
+        private System.Windows.Forms.ToolStripMenuItem btnSad;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cartasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem escolherToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem espadasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ouroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pausToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
